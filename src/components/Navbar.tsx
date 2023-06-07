c390b0035a24adc0f2560d03b46f74897a07e1e8
sudeep sharma <sudip777sharma@gmail.com>
2023-06-07 06:07:01 +0530

    updated the side bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,11 @@ import DarkModeButton from '../globalComponent/DarkModeButton';
 const Navbar = () => {
     return (
         <nav className="fixed t-0 l-0 w-screen bg-gray-900 text-white py-4">
-            <div className="container mx-auto flex items-center justify-between">
+            <div className="container mx-auto flex items-center justify-between pl-12 md:pl-0">
                 <h1 className="text-xl text-center w-full font-semibold hidden md:block">Contact Management App</h1>
                 <div className="flex items-center justify-center space-x-4 w-full">
-                    <input type="text" placeholder="Search" className="p-1 rounded-lg bg-gray-700 text-white" />
+                    <input type="text" placeholder="Search"
+                        className="p-1 w-32 rounded-lg bg-gray-700 text-white" />
                     <div className="p-2 bg-gray-700 rounded-full">
                         <FaSearch className="text-white" />
                     </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
-import { BsFillBox2HeartFill, BsDatabaseFillAdd } from "react-icons/bs";
+import { BsFillBox2HeartFill } from "react-icons/bs";
+import { GrClose } from "react-icons/gr";
+import { RxHamburgerMenu } from 'react-icons/rx';
+
 import { BiHomeSmile } from "react-icons/bi";
 import {
     MdCategory,
-    MdAssignmentAdd,
     MdKeyboardDoubleArrowRight,
 } from "react-icons/md";
-import { RiAdminFill } from "react-icons/ri";
 import Button from "../globalComponent/Button";
 import { useNavigate } from 'react-router-dom';
 
@@ -30,7 +31,15 @@ const sidebarItems = [
 
 const Sidebar = () => {
     const navigate = useNavigate();
+
+    const [screenWidth, setScreenWidth] = React.useState(window.innerWidth);
+    window.addEventListener('resize', () => {
+        setScreenWidth(window.innerWidth);
+    });
+
     const [isHoverActive, setIsHoverActive] = React.useState(false);
+
+    const [isHamBurgerActive, setIsHamBurgerActive] = React.useState(false);
 
     const handleIsHoverActive = () => {
         localStorage.setItem("isHoverActive", JSON.stringify(!isHoverActive));
@@ -42,16 +51,9 @@ const Sidebar = () => {
         setIsHoverActive(isHoverActive === "true" ? true : false);
     }, []);
 
-    return (
-        <div
-            className={`group h-screen cursor-pointer fixed top-0 z-50 flex-col gap-4  py-10 transition-all duration-300 dark:bg-[#0e1421] md:flex ${isHoverActive ? "w-16 hover:w-64" : "w-64"
-                }`}
-        >
-
-            <div
-                className=""
-            >
-                {/* <RxHamburgerMenu/> */}
+    const HoverBtnComp = () => {
+        return (
+            <div>
                 <Button
                     aria-label="Toggle Sidebar"
                     onClick={handleIsHoverActive}
@@ -66,24 +68,68 @@ const Sidebar = () => {
                     />
                 </Button>
             </div>
+        )
+    }
 
-            {sidebarItems.map((item) => (
-                <div
-                    onClick={() => navigate(item.link)}
-                    key={item.name}
-                    className={`mx-2 flex items-center gap-5 rounded-md p-3 text-gray-400 hover:dark:text-gray-500`}
-                >
-                    <span className="text-2xl">{item.icon}</span>
-                    <span
-                        className={`truncate ${isHoverActive ? "hidden group-hover:block" : ""
-                            } `}
+    const SidebarInnerContentComp = () => {
+        return (
+            <>
+                {sidebarItems.map((item) => (
+                    <div
+                        onClick={() => navigate(item.link)}
+                        key={item.name}
+                        className={`mx-2 flex items-center gap-5 rounded-md p-3 text-gray-400 hover:dark:text-gray-500`}
                     >
-                        {item.name}
-                    </span>
-                </div>
-            ))}
+                        <span className="text-2xl">{item.icon}</span>
+                        <span
+                            className={`truncate ${isHoverActive ? "hidden group-hover:block" : ""
+                                } `}
+                        >
+                            {item.name}
+                        </span>
+                    </div>
+                ))}
+            </>
+        )
+    }
+
+    return screenWidth > 425 ? (
+        <div
+            className={`group h-screen cursor-pointer fixed top-0 z-50 flex-col gap-4  py-10 transition-all duration-300 dark:bg-[#0e1421] md:flex ${isHoverActive ? "w-16 hover:w-64" : "w-64"
+                }`}
+        >
+            <HoverBtnComp />
+            <SidebarInnerContentComp />
         </div>
-    );
+    ) : (
+        <div
+            className="fixed z-50"
+        >
+            <RxHamburgerMenu
+                onClick={() => setIsHamBurgerActive((prev) => !prev)}
+                style={{ height: '35px', width: '35px', borderRadius: '5px', backgroundColor: '#374151', color: 'white', padding: '5px' }}
+                className="absolute top-5 left-5 z-50"
+            />
+            {
+                isHamBurgerActive && (
+                    <div
+                        className={`group h-screen cursor-pointer fixed top-0 z-50 flex flex-col gap-4 py-5 transition-all duration-300 dark:bg-[#0e1421] md:flex ${isHoverActive ? "w-16 hover:w-64" : "w-64"
+                            }`}
+                    >
+                        <div
+                            className={`flex items-center justify-end gap-5 rounded-md`}
+                        >
+                            <GrClose
+                                onClick={() => setIsHamBurgerActive((prev) => !prev)}
+                                style={{ height: '35px', width: '35px', borderRadius: '5px', backgroundColor: '#374151', color: 'white', padding: '5px', marginRight: '14px' }}
+                            />
+                        </div>
+                        <SidebarInnerContentComp />
+                    </div>
+                )
+            }
+        </div >
+    )
 };
 
 export default Sidebar;
